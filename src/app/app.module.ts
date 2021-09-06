b9959ef408262eb3ee04ef65a04ef8f8e28831a4
Trinh Tran <kieutrinh.ibc@gmail.com>
2021-09-06 09:52:38 +0300

    project setting up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { SideNavigationComponent } from './side-navigation/side-navigation.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    SideNavigationComponent
+    SideNavigationComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule
